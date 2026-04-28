4d99b268d7fbbff889b15684775a011e4e2253a2
Jo Martins <joaovitor.11.2011@hotmail.com>
2026-04-28 18:09:00 -0300

    feat: melhoria no visual + dashboards + sidebar responsivo

--- src/main/java/com/livraria/controller/HomeController.java
+++ src/main/java/com/livraria/controller/HomeController.java
@@ -1,13 +1,38 @@
 package com.livraria.controller;
 
+import com.livraria.repository.AlunoRepository;
+import com.livraria.repository.LivroRepository;
+import com.livraria.repository.EmprestimoRepository;
+import com.livraria.repository.MultaRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Autowired
+    private LivroRepository livroRepository;
+
+    @Autowired
+    private EmprestimoRepository emprestimoRepository;
+
+    @Autowired
+    private MultaRepository multaRepository;
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+
+        model.addAttribute("totalAlunos", alunoRepository.count());
+        model.addAttribute("totalLivros", livroRepository.count());
+        model.addAttribute("totalEmprestimos", emprestimoRepository.count());
+        model.addAttribute("totalMultas", multaRepository.count());
+
         return "index";
     }
 }
